70740a4fbd99609e4d209deeb75603ccf03785f1
Florian Herrmann <herrmann.florian@mailbox.org>
2021-04-14 14:13:48 +0200

    Solved third puzzle

--- src/day03/dayThree.groovy
+++ src/day03/dayThree.groovy
@@ -20,10 +20,32 @@ List<String> testInput = [
 
 int treeCounter1 = countTrees(testInput, 0, 3, 1)
 assert treeCounter1 == 7
+int treeCounter2 = countTrees(testInput, 0, 1, 1)
+assert treeCounter2 == 2
+int treeCounter3 = countTrees(testInput, 0, 5, 1)
+assert treeCounter3 == 3
+int treeCounter4 = countTrees(testInput, 0, 7, 1)
+assert treeCounter4 == 4
+int treeCounter5 = countTrees(testInput, 0, 1, 2)
+assert treeCounter5 == 2
+long counterProduct = treeCounter1 * treeCounter2 * treeCounter3 * treeCounter4 * treeCounter5
+assert counterProduct == 336
 
 List<String> input = new File("inputThree.txt").readLines()
-int result1 = countTrees(input, 0, 3 ,1)
+int result1 = countTrees(input, 0, 3, 1)
 println("In the main slope there are $result1 trees.")
+List<Integer> slopes = [
+        countTrees(input, 0, 1, 1),
+        countTrees(input, 0, 3, 1),
+        countTrees(input, 0, 5, 1),
+        countTrees(input, 0, 7, 1),
+        countTrees(input, 0, 1, 2)
+]
+long product = 1L
+slopes.forEach { trees ->
+    product *= trees
+}
+println("Product of trees on each slope is $product")
 
 /**
  * <p>Consumes an input List and iterates from top to bottom in a slope with
