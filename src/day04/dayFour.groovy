99eb30ab3f4893b632d00d23c92675e4d85ebdc5
Florian Herrmann <herrmann.florian@mailbox.org>
2021-04-15 18:39:52 +0200

    Some refactoring and solved fourth puzzle

--- src/day04/dayFour.groovy
+++ src/day04/dayFour.groovy
@@ -1,47 +1,59 @@
 package day04
 
-import groovy.transform.Field
-
-@Field static final List<String> RELEVANT_FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"] // cid
-
 List<String> testInput = new File("testFour.txt").readLines()
 int counter1 = countCompleteEntries(testInput)
 assert counter1 == 2
+List<String> testInput2 = new File("testFour2.txt").readLines()
+int counter2 = countValidEntries(testInput2)
+assert counter2 == 4
 
 List<String> input = new File("inputFour.txt").readLines()
 int result1 = countCompleteEntries(input)
-println( "The passport List contains $result1 complete passports")
+println("The passport List contains $result1 complete passports")
+int result2 = countValidEntries(input)
+println("The passport List contains $result2 valid passports")
 
 /**
- * <p>Checks every passport line if it contains all of the relevant fields.</p>
+ * <p>Checks every passport if it contains all of the relevant fields.</p>
  * <p>"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm" will result <b>true</b>,<br/>
  * "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in" misses "byr" and will result <b>false</b>.</p>
  *
  * @return the count of passports containing all relevant fields
  */
 static int countCompleteEntries(List<String> input) {
-    List<String> passports = packLinesToPassports(input)
+    List<Passport> passports = packLinesToPassports(input)
 
     return passports.count { passport ->
-        RELEVANT_FIELDS.every { field ->
-            passport.contains(field)
-        }
+        passport.isComplete()
+    }
+}
+
+/**
+ * <p>Checks every passport if it matches all validity criteria.</p>
+ *
+ * @return the count of passports that are valid by criteria
+ */
+static int countValidEntries(List<String> lines) {
+    List<Passport> passports = packLinesToPassports(lines)
+
+    return passports.count { passport ->
+        passport.isComplete() && passport.isValid()
     }
 }
 
 /**
  * <p>Consumes a List of possibly multiline passports separated by empty lines
- * and transforms it to a List of passport oneliners.</p>
+ * and transforms it to a List of {@link day04.Passport}</p>
  *
  * @return a List of passport oneliners
  */
-static List<String> packLinesToPassports(List<String> lines) {
+static List<Passport> packLinesToPassports(List<String> lines) {
     List<String> buffer = []
-    List<String> passports = []
+    List<Passport> passports = []
 
     lines.each { line ->
         if (line.isEmpty()) {
-            passports.add(bufferToString(buffer))
+            passports.add(new Passport(bufferToString(buffer)))
             buffer.clear()
         } else {
             buffer.add(line)
@@ -49,7 +61,7 @@ static List<String> packLinesToPassports(List<String> lines) {
     }
     // since a passport List doesn't end with an empty line we have to add the last entry manually like this
     if (!buffer.isEmpty()) {
-        passports.add(bufferToString(buffer))
+        passports.add(new Passport(bufferToString(buffer)))
     }
 
     return passports
