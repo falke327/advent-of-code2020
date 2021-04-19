9d3786a7258f52c474a011dc040e241d1951827b
Florian Herrmann <herrmann.florian@mailbox.org>
2021-04-19 18:54:26 +0200

    Simplified day06

--- src/day06/daySix.groovy
+++ src/day06/daySix.groovy
@@ -1,66 +1,54 @@
 package day06
 
-List<String> testInput = new File("testSix.txt").readLines()
+List<String> testInput = new File("testSix.txt").text.split(/\r\n\r\n/)
 assert countLettersForGroups(testInput).sum() == 11
 assert countIntersectsForGroups(testInput).sum() == 6
 
-List<String> input = new File("inputSix.txt").readLines()
+List<String> input = new File("inputSix.txt").text.split(/\r\n\r\n/)
 int result1 = countLettersForGroups(input).sum() as int
 println("The overall customs sum is $result1")
 int result2 = countIntersectsForGroups(input).sum() as int
 println("The overall count of intersections is $result2")
 
 /**
- * <p>Consumes a List of answers in groups separated by empty lines
+ * <p>Consumes a List of answers in groups split by empty lines
  * and counts the distinct digits for each group</p>
  * <p>[ab, acd] -> ab + acd -> abcd -> 4</p>
  *
  * @return a List counts for each group
  */
 static List<Integer> countLettersForGroups(List<String> lines) {
-    Set<String> buffer = []
     List<Integer> groups = []
+    lines.each {line ->
+        Set<String> buffer = []
+        List<String> personAnswers = line.split()
 
-    lines.each { line ->
-        if (line.isEmpty()) {
-            groups.add(new HashSet(buffer).size())
-            buffer.clear()
-        } else {
-            buffer.addAll(line.toSet())
+        personAnswers.each {answer ->
+            buffer.addAll(answer.toSet())
         }
-    }
-    // since a group List doesn't end with an empty line we have to add the last entry manually like this
-    if (!buffer.isEmpty()) {
         groups.add(new HashSet(buffer).size())
     }
-
     return groups
 }
 
 /**
- * <p>Consumes a List of answers in groups separated by empty lines
+ * <p>Consumes a List of answers in groups split by empty lines
  * and counts the intersect digits for each group</p>
  * <p>[ab, acd] -> ab + acd -> a -> 1</p>
  *
  * @return a List counts for each group
  */
 static List<Integer> countIntersectsForGroups(List<String> lines) {
-    List<List> buffer = []
     List<Integer> groups = []
-
     lines.each { line ->
-        if (line.isEmpty()) {
-            groups.add(new HashSet(getOverallIntersection(buffer)).size())
-            buffer.clear()
-        } else {
-            buffer.add(line.toList())
-        }
-    }
-    // since a group List doesn't end with an empty line we have to add the last entry manually like this
-    if (!buffer.isEmpty()) {
-        groups.add(new HashSet(getOverallIntersection(buffer)).size())
-    }
+        List<List> bufferedAnswers = []
+        List<String> personAnswers = line.split()
 
+        personAnswers.each {answer ->
+            bufferedAnswers.add(answer.toList())
+        }
+        groups.add(new HashSet(getOverallIntersection(bufferedAnswers)).size())
+    }
     return groups
 }
 
@@ -70,10 +58,10 @@ static List<Integer> countIntersectsForGroups(List<String> lines) {
  *
  * @return the intersected List
  */
-static Set<String> getOverallIntersection(List<List> buffer) {
+static Set<String> getOverallIntersection(List<List> bufferedAnswers) {
     Set<String> result = ('a'..'z').toList()
-    buffer.each {
-        result = result.intersect(it)
+    bufferedAnswers.each {answer->
+        result = result.intersect(answer)
     }
     return result
 }
