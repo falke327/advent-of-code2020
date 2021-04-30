e6309f8641305e0b2f29e8fa4343a7cda90b9127
Florian Herrmann <herrmann.florian@mailbox.org>
2021-04-30 13:03:04 +0200

    Cleaned up the comments and refactored names

--- src/day09/dayNine.groovy
+++ src/day09/dayNine.groovy
@@ -5,7 +5,7 @@ assert 127 == findFailingXmasCode(testInput, 5)
 
 List<String> reducedTest = testInput.subList(0, testInput.indexOf(127 as String))
 List partialTest2 = findSumList(reducedTest, 127)
-assert 62 == getEncryptionWeakness(partialTest2)
+assert 62 == calculateEncryptionWeakness(partialTest2)
 
 List<String> input = new File("inputNine.txt").readLines()
 int result1 = findFailingXmasCode(input, 25)
@@ -14,14 +14,14 @@ println("Failing Xmas code found. Value is $result1")
 List<String> reducedInput = input.subList(0, input.indexOf(result1 as String))
 List partialResult2 = findSumList(reducedInput, result1)
 println(partialResult2)
-int result2 = getEncryptionWeakness(partialResult2)
+int result2 = calculateEncryptionWeakness(partialResult2)
 println("Found weakness with value $result2")
 
 
 /**
  * <p>Checks each entry in a List after the offset if it is in xmas code.</p>
  *
- * @return the value that doesn't match, or -1 if not found
+ * @return the value that doesn't match<br/>-1 if not found
  */
 static int findFailingXmasCode(List input, int offset) {
     int startIndex = 0
@@ -39,7 +39,7 @@ static int findFailingXmasCode(List input, int offset) {
 }
 
 /**
- * <p>Checks if checkValue can be build as sum of two List entries</p>
+ * A Value is in Xmas code if it can be build as a sum of two values from given List
  */
 static boolean isXmasCode(List subList, int checkValue) {
     for (int a = 0; a < subList.size() - 1; a++) {
@@ -55,29 +55,29 @@ static boolean isXmasCode(List subList, int checkValue) {
 }
 
 /**
- * <p>Searches for a sublist that sums to the checkValue.</p>
- * <p>Increases sublist size after each iteration, starting with 3</p>
+ * <p>Searches for a sublist that sums to the checkValue, by windowing the original List.</p>
+ * <p>The starting window size is 3 an increases after each iteration</p>
  *
- * @return a matching sublist or empty List if not found
+ * @return a matching sublist<br/>empty List if not found
  */
 static List findSumList(List input, int checkValue) {
-    int sublistSize = 3
-    while(sublistSize < input.size()) {
-        for (int i = 0; i + sublistSize < input.size(); i++) {
-            List currentSublist = input.subList(i, i + sublistSize)
+    int windowSize = 3
+    while(windowSize < input.size()) {
+        for (int i = 0; i + windowSize < input.size(); i++) {
+            List currentSublist = input.subList(i, i + windowSize)
             if (currentSublist.sum { it as int } == checkValue) {
                 return currentSublist
             }
         }
-        sublistSize++
+        windowSize++
     }
     return []
 }
 
 /**
- * Adds the List minimum to the List maximum
+ * The weakness is defined as the List minimum added to the List maximum
  */
-static int getEncryptionWeakness(List input) {
+static int calculateEncryptionWeakness(List input) {
     int minimum = input.min() as int
     int maximum = input.max() as int
     return minimum + maximum
