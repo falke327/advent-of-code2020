67917a34281ebf0a1506dedcb6fb972b7eddd83c
Florian Herrmann <herrmann.florian@mailbox.org>
2021-05-11 11:29:29 +0200

    Solved part two

--- src/day11/dayEleven.groovy
+++ src/day11/dayEleven.groovy
@@ -1,25 +1,42 @@
 package day11
 
 List<String> testInput = new File("testEleven.txt").readLines()
-assert 37 == countOccupiedSeatsInStableStatus(testInput)
+WaitingArea testWaitingArea = new WaitingArea(testInput)
+WaitingArea testWaitingArea2 = new WaitingArea(testInput)
+assert 37 == countOccupiedSeatsInStableStatusByNeighborRule(testWaitingArea)
+assert 26 == countOccupiedSeatsInStableStatusByVisibleRule(testWaitingArea2)
 
 List<String> input = new File("inputEleven.txt").readLines()
-int result1 = countOccupiedSeatsInStableStatus(input)
-println("There are $result1 occupied seats in the stable state")
+WaitingArea waitingArea = new WaitingArea(input)
+int result1 = countOccupiedSeatsInStableStatusByNeighborRule(waitingArea)
+println("There are $result1 occupied seats for neighbor rule")
+WaitingArea waitingArea2 = new WaitingArea(input)
+int result2 = countOccupiedSeatsInStableStatusByVisibleRule(waitingArea2)
+println("There are $result2 occupied seats for visible rule")
 
-static int countOccupiedSeatsInStableStatus(List<String> seatArrangement) {
-    WaitingArea waitingArea = new WaitingArea(seatArrangement)
-
-    return calculateStableStatusOccupiedSeats(waitingArea)
-}
-
-static int calculateStableStatusOccupiedSeats(WaitingArea waitingArea) {
+/**
+ * In Neighbor rule every seat is only affected by its adjacent seats
+ */
+static int countOccupiedSeatsInStableStatusByNeighborRule(WaitingArea waitingArea) {
     List<String> oldStatus = []
     List<String> currentIteration = waitingArea.getCurrentStatus()
 
     while (oldStatus != currentIteration) {
         oldStatus = currentIteration
         waitingArea.updateAllSeatsByNeighbor()
+        currentIteration = waitingArea.getCurrentStatus()
+    }
+
+    return waitingArea.countOccupiedSeats()
+}
+
+static int countOccupiedSeatsInStableStatusByVisibleRule(WaitingArea waitingArea) {
+    List<String> oldStatus = []
+    List<String> currentIteration = waitingArea.getCurrentStatus()
+
+    while (oldStatus != currentIteration) {
+        oldStatus = currentIteration
+        waitingArea.updateAllSeatsByVisibility()
         currentIteration = waitingArea.getCurrentStatus()
     }
 
