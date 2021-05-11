19f7ee2f043134a7c4d80fc774ead8596dde6e8a
Florian Herrmann <herrmann.florian@mailbox.org>
2021-05-11 14:28:19 +0200

    Solved second part

--- src/day12/dayTwelve.groovy
+++ src/day12/dayTwelve.groovy
@@ -1,18 +1,32 @@
 package day12
 
 List<String> testInput = new File("testTwelve.txt").readLines()
-assert 25 == calculateManhattenDistance(testInput)
+assert 25 == calculateManhattenDistanceForShip(testInput)
+assert 286 == calculateManhattenDistanceForWaypoint(testInput)
 
 List<String> input = new File("inputTwelve.txt").readLines()
-int result1 = calculateManhattenDistance(input)
-println("The manhatten distance for given command set is $result1")
+int result1 = calculateManhattenDistanceForShip(input)
+println("The manhatten distance for given command set by ship is $result1")
 
-static int calculateManhattenDistance(List<String> commands) {
+int result2 = calculateManhattenDistanceForWaypoint(input)
+println("The manhatten distance for given command set by waypoint is $result2")
+
+static int calculateManhattenDistanceForShip(List<String> commands) {
     Ship ship = new Ship(Direction.EAST)
 
-    commands.each {command->
+    commands.each { command ->
         ship.performMovementCommand(command)
     }
 
     return ship.getManhattenDistance()
+}
+
+static int calculateManhattenDistanceForWaypoint(List<String> commands) {
+    Waypoint waypoint = new Waypoint(-10, -1)
+
+    commands.each { command ->
+        waypoint.performMovementCommand(command)
+    }
+
+    return waypoint.getManhattenDistance()
 }
